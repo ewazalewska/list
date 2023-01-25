a709dcb5abf0bf34575649e5885f6165c7ed20d3
Ewa <73430438+ewazalewska@users.noreply.github.com>
2023-01-25 22:58:56 +0100

    Add files via upload

---
+++ range.py
@@ -0,0 +1,9 @@
+# range(start, stop, step) default: start = 0, step = 1
+# python 2.x - function(argument) -> list
+# python 3.x - data type(parameters) -> sequence of numbers (more efficient)
+
+
+A = list(range(10))
+print(A)
+B = list(range(100,0,-3))
+print(B)
